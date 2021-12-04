cc01615aa4f18ebacd321bf43e42506c83319c9d
Haku Kazamaru <kil-dunamis@live.jp>
2021-12-04 23:24:41 +0900

    プロジェクト ファイルを追加します。

---
+++ haku-chat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace haku_chat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
